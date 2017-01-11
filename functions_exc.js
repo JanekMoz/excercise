1c04edc1a1aa7aa86159657cf546f3c8fe010ea9
JanekMoz <mozyr12@gmail.com>
2017-01-11 16:47:39 +0100

    added file

---
+++ functions_exc.js
@@ -0,0 +1,11 @@
+getTriangleArea 
+function getTriangleArea(a, h) {
+(a, h > 0)
+if (a, h <= 0) {
+"Nieprawidłowe dane"
+}
+if (a, h > 0) {
+return a*h/2
+}    
+}
+console.log( getTriangleArea(10,6) )
